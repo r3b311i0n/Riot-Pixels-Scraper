c9cc80c1dd20c5f827e1f519e40cae42ce52af73
r3b311i0n <nasashinega@gmail.com>
2017-07-13 22:09:25 +0530

    Add basic company info

--- src/company.ts
+++ src/company.ts
@@ -3,21 +3,36 @@ import {IncomingMessage} from 'http';
 import {get} from 'request';
 import {Scraper} from './scraper';
 
+interface ICompanyObject {
+    title: string;
+    cover: string;
+    platforms: string[];
+}
+
 export default class Company extends Scraper {
     constructor(url: string) {
-        super('http://en.riotpixels.com/companies/' + url.replace(/\W/, '-').toLowerCase().slice(1));
+        super('http://en.riotpixels.com/companies/' + url.replace(/\W/, '-').toLowerCase());
     }
 
-    public scrape(): Promise<{} | number> {
+    public scrape(): Promise<ICompanyObject | number> {
         return new Promise((resolve, reject) => {
             get(this.url, (err: object, response: IncomingMessage, body: string) => {
                 if (response.statusCode > 399 || response.statusCode < 200) {
                     reject(err);
                 }
 
-                const $ = cheerio.load(body);
+                this.$ = cheerio.load(body);
 
-                resolve();
+                const title = this.$('.company-title').text().trim();
+                const cover = this.$('.company-logo img').attr('src');
+
+                // Get platforms.
+                const platforms: string[] = this.getPlatforms();
+
+                // Final company object.
+                const company = {title, cover, platforms};
+
+                resolve(company);
             });
         });
     }
